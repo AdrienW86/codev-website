16b098b6ceafde2ba7e316af2b96e7bbe5c6379a
Adrien <adrien_weiss@outlook.fr>
2023-01-05 15:53:08 +0100

    mise en ligne

---
+++ components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Image from 'next/image'
+
+import styles from '../styles/Home.module.css'
+
+ const Card = (props) => {
+
+  return (
+    <a href={props.url} className={styles.card}>
+      <h2> {props.title} &rarr;</h2>
+        <Image
+          width={100}
+          height= {100}
+          src= {props.template}
+          priority='blur'
+          layout='responsive'
+          alt= 'photo du template'
+        />
+    </a>
+  )
+}
+
+export default Card
